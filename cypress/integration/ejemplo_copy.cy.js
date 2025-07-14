ac0e102d39c73f66ddb48665725632062798ccd4
ManuOlivares <man.olivaresr@gmail.com>
2025-07-14 17:36:06 -0400

    correcion

--- cypress/integration/ejemplo_copy.cy.js
+++ cypress/integration/ejemplo_copy.cy.js
@@ -1,41 +1,29 @@
 ///<reference types="cypress" />
 import 'cypress-plugin-tab';
 import 'cypress-file-upload';
-import 'cypress-plugin-tab';
 import 'cypress-xpath';
 
 describe("Reto de tablas con funciones", () => {
+  it("automatizar test biblioteca utilizando funciones", () => {
+    const tiempo = 2000;
+    cy.visit("https://demoqa.com/books");
+    cy.wait(tiempo);
+    cy.title().should('eq', 'DEMOQA');
 
-    it("automatizar test biblioteca utilizando funciones ", ()=> {
-        const tiempo = 2000;
-        cy.visit("https://demoqa.com/books")
-        cy.wait(tiempo)
-        cy.title().should('eq', 'DEMOQA')
+    cy.get(".rt-td").each(($el, i) => {
+      const textoCelda = $el.text();
 
-        const datos=[]
-        const campo=cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(2)')
+      cy.log(`Fila ${i + 1}: ${textoCelda}`);
 
-        cy.get(".rt-td").each(($el, index, $list) => {
-            datos[index] = $el.text()
-        }).then(() => {                                
-            for(let i = 0; i < datos.length; i++){
-                cy.log(datos[i])
-            }
-           // cy.log(`Total de elementos: ${datos.length}`)
-
-            if(datos.includes('Git Pocket Guide')){
-                campo.eq(index).next().next().then((author) =>{
-                    const Book=author.text()
-                    cy.log(`El autor es: ${Book}`)
-
-
-
-                })
-            
-            }
-
-        })
-    
+      if (textoCelda.includes("Git Pocket Guide")) {
+        cy.get('.rt-tbody > :nth-child(1) > .rt-tr')
+          .eq(i)
+          .find(':nth-child(4)')
+          .then(($author) => {
+            const autor = $author.text();
+            cy.log(`🔍 El autor de "Git Pocket Guide" es: ${autor}`);
+          });
+      }
     });
-
+  });
 });
